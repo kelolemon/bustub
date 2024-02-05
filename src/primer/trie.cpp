eb190df5380c83706a687997e3d047c67e4c2758
dengmengda.cd <dengmengda.cd@bytedance.com>
2024-02-05 20:28:09 +0800

    [feat]: complete get, put haven't cow

--- src/primer/trie.cpp
+++ src/primer/trie.cpp
@@ -1,4 +1,6 @@
 #include "primer/trie.h"
+#include <cstddef>
+#include <memory>
 #include <string_view>
 #include "common/exception.h"
 
@@ -6,8 +8,22 @@ namespace bustub {
 
 template <class T>
 auto Trie::Get(std::string_view key) const -> const T * {
-  throw NotImplementedException("Trie::Get is not implemented.");
-
+  auto root = this->GetRoot();
+  auto now_node = std::const_pointer_cast<TrieNode>(root);
+  for (const auto &c : key) {
+    if (now_node->children_.find(c) == now_node->children_.end()) {
+      return nullptr;
+    }
+    now_node = std::const_pointer_cast<TrieNode>(now_node->children_.at(c));
+  }
+  if (!now_node->is_value_node_) {
+    return nullptr;
+  }
+  auto node = std::dynamic_pointer_cast<const TrieNodeWithValue<T>>(now_node);
+  if (node == nullptr) {
+    return nullptr;
+  }
+  return node->value_.get();
   // You should walk through the trie to find the node corresponding to the key. If the node doesn't exist, return
   // nullptr. After you find the node, you should use `dynamic_cast` to cast it to `const TrieNodeWithValue<T> *`. If
   // dynamic_cast returns `nullptr`, it means the type of the value is mismatched, and you should return nullptr.
@@ -17,8 +33,19 @@ auto Trie::Get(std::string_view key) const -> const T * {
 template <class T>
 auto Trie::Put(std::string_view key, T value) const -> Trie {
   // Note that `T` might be a non-copyable type. Always use `std::move` when creating `shared_ptr` on that value.
-  throw NotImplementedException("Trie::Put is not implemented.");
-
+  auto root = this->GetRoot();
+  auto new_root = root->Clone();
+  auto now_node = std::const_pointer_cast<TrieNode>(root);
+  auto now_now_node = std::make_shared<TrieNode>(new_root);
+  for (const auto &c : key) {
+    if (now_node->children_.find(c) == now_node->children_.end()) {
+      now_node->children_.insert({c, std::make_shared<TrieNode>()});
+    }
+    now_node = std::const_pointer_cast<TrieNode>(now_node->children_.at(c));
+  }
+  now_node = std::dynamic_pointer_cast<TrieNode>(std::make_shared<TrieNodeWithValue<T>>(new TrieNodeWithValue<T>(std::make_shared<T>(value))));
+  now_node->is_value_node_ = true;
+  return Trie(root);
   // You should walk through the trie and create new nodes if necessary. If the node corresponding to the key already
   // exists, you should create a new `TrieNodeWithValue`.
 }
